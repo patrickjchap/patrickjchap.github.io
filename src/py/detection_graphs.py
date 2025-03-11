6b1d882974cce45ef45b876b2b8b6be9a4d30dea
patrickjchap <pchapman@ucdavis.edu>
2025-03-11 06:50:16 -0700

    Helper files

---
+++ src/py/detection_graphs.py
@@ -0,0 +1,8 @@
+
+def generate_code_bleu_plot(csv_filepath: str, output_path: str):
+
+def generate_pass_k_plot(csv_filepath: str, output_path: str):
+
+def generate_repair_summary_plot(csv_filepath: str, output_path: str):
+
+
