487614be0586434b453c7208b3049f26d5161061
patrickjchap <pchapman@ucdavis.edu>
2025-03-11 15:20:58 -0700

    Setting up plotly interactive graphs for site

--- src/py/detection_graphs.py
+++ src/py/detection_graphs.py
@@ -1,8 +1,23 @@
+import json
+from datetime import datetime
 
-def generate_code_bleu_plot(csv_filepath: str, output_path: str):
+import plotly.express as px
+import plotly.io as io
 
-def generate_pass_k_plot(csv_filepath: str, output_path: str):
+DATE_FORMAT = "%Y-%m-%dT%H:%M:%SZ"
 
-def generate_repair_summary_plot(csv_filepath: str, output_path: str):
+def generate_detection_rate_timeline(json_filepath: str, output_path: str):
+    with open(json_filepath) as f:
+        detection_results = json.load(f)
 
+    for artifact_id, evaluation_results in detection_results.items():
+        for evaluation_method, results in evaluation_results.items():
+        
+
+    return True
+
+#def generate_pass_k_plot(csv_filepath: str, output_path: str):
+#
+#def generate_repair_summary_plot(csv_filepath: str, output_path: str):
+#
 
